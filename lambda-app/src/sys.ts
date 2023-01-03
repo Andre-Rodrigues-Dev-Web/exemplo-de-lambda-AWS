cd1947cb5a48789a948c393a6d99fe00b56b4a7d
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2023-01-03 14:19:12 -0300

    feat: update função

--- lambda-app/src/sys.ts
+++ lambda-app/src/sys.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 //Cria uma função assíncrona que gera um arquivo html
 export async function handler() {
     //Crie uma pasta chamada 'files' dentro da pasta src
-    const file = fs.createWriteStream('C:/Users/Andre/Documents/GitHub/exemplo-de-lambda-AWS/lambda-app/src/files/index.html');
+    const file = fs.createWriteStream('./files/index.html');
     //Escreva o conteúdo do arquivo
     file.write('<h1>Hello World</h1>');
     //Feche o arquivo
@@ -10,7 +10,7 @@ export async function handler() {
     //Try catch para verificar se o arquivo foi criado
     try {
         //Retorna o arquivo
-        return fs.readFileSync('C:/Users/Andre/Documents/GitHub/exemplo-de-lambda-AWS/lambda-app/src/files/index.html', 'utf8');
+        return fs.readFileSync('./files/index.html', 'utf8');
     }
     catch (err) {
         //Retorna o erro
